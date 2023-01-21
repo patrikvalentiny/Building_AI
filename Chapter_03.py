8522c631511525d1128c007da6011a477d3a5f59
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-21 18:44:42 +0100

    E3 Advanced

--- Chapter_03.py
+++ Chapter_03.py
@@ -2,9 +2,10 @@ import math
 import random             	# just for generating random mountains
 
 # generate random mountains
-w = [random.random()/3, random.random()/3, random.random()/3]
-h = [1.+math.sin(1+x/6.)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
-h[0] = 0.0; h[99] = 0.0
+
+w = [.05, random.random()/3, random.random()/3]
+h = [1.+math.sin(1+x/.6)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
+
 
 def climb(x, h):
     # keep climbing until we've found a summit
@@ -13,22 +14,18 @@ def climb(x, h):
     # edit here
     while not summit:
         summit = True         # stop unless there's a way up
-        if h[x + 1] > h[x]:
-            x = x + 1         # right is higher, go there
-            summit = False    # and keep going
-        elif h[x-1] > h[x]:
-            x -= 1
-            summit = False
+        for i in range(max(0, x-5), min(99, x+5)):
+            if h[i] > h[x]:
+                x = i       # right is higher, go there
+                summit = False    # and keep going
     return x
 
 
 def main(h):
-
     # start at a random place
     x0 = random.randint(1, 98)
     x = climb(x0, h)
 
-    print("Venla started at %d and got to %d" % (x0, x))
     return x0, x
 
 main(h)
