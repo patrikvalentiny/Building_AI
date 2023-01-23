4b2337ce8f37c9a73bdadcee3afefdbe8c14fa3b
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-23 13:10:09 +0100

    E10 Intermediate

--- Chapter_2_02.py
+++ Chapter_2_02.py
@@ -1,12 +1,8 @@
-def bot8(pbot, p8_bot, p8_human):
-    phuman = 1 - pbot
-    p8 = pbot * p8_bot + phuman *p8_human
-    pbot_8 = p8_bot * pbot / p8
-    print(pbot_8)
+def flip(n):
+    odds = 1.0         # start with 50% chance of the magic coin, which is the same as odds = 1:1
+    for i in range(n):
+        odds *= 2           # edit here to update the odds
+    print(odds)
 
-# you can change these values to test your program with different values
-pbot = 0.1
-p8_bot = 0.8
-p8_human = 0.05
-
-bot8(pbot, p8_bot, p8_human)
+n = 3
+flip(n)
