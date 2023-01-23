8338860077a65c825385d9b023ec83bb5bdfbf0c
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-23 13:14:41 +0100

    E10 Advanced

--- Chapter_2_02.py
+++ Chapter_2_02.py
@@ -1,8 +1,42 @@
-def flip(n):
-    odds = 1.0         # start with 50% chance of the magic coin, which is the same as odds = 1:1
-    for i in range(n):
-        odds *= 2           # edit here to update the odds
-    print(odds)
+import numpy as np
 
-n = 3
-flip(n)
+p1 = [1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6]  # normal
+p2 = [0.1, 0.1, 0.1, 0.1, 0.1, 0.5]  # loaded
+
+
+def roll(loaded):
+    if loaded:
+        print("rolling a loaded die")
+        p = p2
+    else:
+        print("rolling a normal die")
+        p = p1
+
+    # roll the dice 10 times
+    # add 1 to get dice rolls from 1 to 6 instead of 0 to 5
+    sequence = np.random.choice(6, size=10, p=p) + 1
+    for roll in sequence:
+        print("rolled %d" % roll)
+
+    return sequence
+
+
+def bayes(sequence):
+    odds = 1.0  # start with odds 1:1
+    for roll in sequence:
+        if roll == 6:
+            r = (1/2) / (1/6)
+        else:
+            r = (1/10) /  (1/6)
+        odds *= r # edit here to update the odds
+    if odds > 1:
+        return True
+    else:
+        return False
+
+
+sequence = roll(True)
+if bayes(sequence):
+    print("I think loaded")
+else:
+    print("I think normal")
