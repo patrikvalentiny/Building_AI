4cd2b8b2f13e10010fc9c96fa03045db2b7dc157
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-28 17:39:09 +0100

    E12 Advanced

--- Chapter_3_01.py
+++ Chapter_3_01.py
@@ -1,10 +1,28 @@
 import numpy as np
 
+# data
 X = np.array([[66, 5, 15, 2, 500],
               [21, 3, 50, 1, 100],
               [120, 15, 5, 2, 1200]])
 y = np.array([250000, 60000, 525000])
-c = np.array([3000, 200, -50, 5000, 100])  # coefficient values
+
+# alternative sets of coefficient values
+c = np.array([[3000, 200 , -50, 5000, 100],
+              [2000, -250, -100, 150, 250],
+              [3000, -100, -150, 0, 150]])
+
+def find_best(X, y, c):
+    smallest_error = np.Inf
+    best_index = -1
+    for i, coeff in enumerate(c):
+        # edit here: calculate the sum of squared error with coefficient set coeff and
+        # keep track of the one yielding the smallest squared error
+        sse = squared_error(X, y, coeff)
+        if sse < smallest_error:
+            smallest_error = sse
+            best_index = i
+
+    print("the best set is set %d" % best_index)
 
 
 def squared_error(X, y, c):
@@ -19,7 +37,7 @@ def squared_error(X, y, c):
         # and add up all the differences in variable sse
         sse += diff
 
-    print(sse)
+    return sse
 
 
-squared_error(X, y, c)
+find_best(X, y, c)
