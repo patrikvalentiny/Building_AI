4d7dc9b45439241eeaa321734785977b13febdc4
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-28 17:09:10 +0100

    E12 Intermediate

--- Chapter_3_01.py
+++ Chapter_3_01.py
@@ -1,24 +1,25 @@
-# input values for three mökkis:
-#  - size [m^2],
-#  - size of the sauna [m^2],
-#  - distance to water [m],
-#  - number of indoor bathrooms,
-#  - proximity of neighbors [m]
-X = [[66, 5, 15, 2, 500],
-     [21, 3, 50, 1, 100],
-     [120, 15, 5, 2, 1200]]
+import numpy as np
 
-# coefficient values
-c = [3000, 200, -50, 5000, 100]
+X = np.array([[66, 5, 15, 2, 500],
+              [21, 3, 50, 1, 100],
+              [120, 15, 5, 2, 1200]])
+y = np.array([250000, 60000, 525000])
+c = np.array([3000, 200, -50, 5000, 100])  # coefficient values
 
-def predict(X, c):
-    # write a loop that goes over the cabin data and for each cabin prints out
-    # the predicted price of the cabin you can assume that the number of inputs
-    # and the number of coefficients are the same
-    for i in X:
-        price = 0
-        for j in range(len(i)):
-            price += i[j] * c[j]
-        print(price)
 
-predict(X, c)
+def squared_error(X, y, c):
+    sse = 0.0
+    for xi, yi in zip(X, y):
+        # add your code here: calculate the predicted price,
+        diff = np.dot(xi, c)
+        # subtract it from the actual price yi,
+        diff = yi - diff
+        # square the difference using (yi - prediction)**2,
+        diff = diff ** 2
+        # and add up all the differences in variable sse
+        sse += diff
+
+    print(sse)
+
+
+squared_error(X, y, c)
