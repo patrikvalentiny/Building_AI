4f21cde0a89845bd962bdce78abd6b618c9191d2
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-02-02 23:23:57 +0100

    E18 Intermediate

--- Chapter_3_03.py
+++ Chapter_3_03.py
@@ -1,26 +1,38 @@
-import numpy as np
+# DATA BLOCK
 
-data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+text = '''he really really loves coffee
+my sister dislikes coffee
+my sister loves tea'''
 
+import math
 
-def find_nearest_pair(data):
-    N = len(data)
-    dist = np.empty((N, N), dtype=np.single)
-    for i in range(N):
-        for j in range(N):
-            sum = 0
-            if i == j:
-                sum = np.inf
-            else:
-                for k in range(len(data[i])):
-                    sum += abs(data[i][k] - data[j][k])
-            dist[i, j] = sum
+def main(text):
+    # split the text first into lines and then into lists of words
+    docs = [line.split() for line in text.splitlines()]
 
-    print(np.unravel_index(np.argmin(dist), dist.shape))
+    N = len(docs)
 
+    # create the vocabulary: the list of words that appear at least once
+    vocabulary = list(set(text.split()))
 
-find_nearest_pair(data)
+    df = {}
+    tf = {}
+    for word in vocabulary:
+        # tf: number of occurrences of word w in document divided by document length
+        # note: tf[word] will be a list containing the tf of each word for each document
+        # for example tf['he'][0] contains the term frequence of the word 'he' in the first
+        # document
+        tf[word] = [doc.count(word)/len(doc) for doc in docs]
+
+        # df: number of documents containing word w
+        df[word] = sum([word in doc for doc in docs])/N
+    # loop through documents to calculate the tf-idf values
+    for doc_index, doc in enumerate(docs):
+        tfidf = []
+        for word in vocabulary:
+            # ADD THE CORRECT FORMULA HERE. Remember to use the base 10 logarithm: math.log(x, 10)
+            tfidf.append(tf[word][doc_index] * math.log(1 / df[word], 10))
+
+        print(tfidf)
+
+main(text)
